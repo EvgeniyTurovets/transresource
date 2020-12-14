1f04da88e243c3c3ab577a494b23259f2a5e37f3
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2020-12-14 16:41:16 +0200

    1
    1

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+    // видео
     let video = $('.video-block video')
     video.on('ended',function(){
         $(this).closest('.video-block').addClass('active')
@@ -11,4 +12,12 @@ $(function(){
         thisVideo.trigger('play');
          console.log('123')
     })
+
+
+    // табы
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+        $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').hide().removeClass('active').eq($(this).index()).fadeIn().addClass('active');
+    });
 })
